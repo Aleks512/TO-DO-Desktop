872ffd1e0283d14bc1c7db7911dfb449a4688592
Aleksandra Cichocka <54075858+Aleks512@users.noreply.github.com>
2023-05-30 21:17:45 +0200

    returnPressed signal

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ class MainWindow(QWidget):
         self.setWindowTitle("Aleks TO DO")
         main_layout = QVBoxLayout()
         list_widget = QListWidget()
+        self.setLayout(main_layout)
         edit_widget = QLineEdit()
         edit_widget.setPlaceholderText("à faire..")
         btn_to_clear = QPushButton("Supprimer")
@@ -16,7 +17,13 @@ class MainWindow(QWidget):
         main_layout.addWidget(edit_widget)
         main_layout.addWidget(btn_to_clear)
 
-        self.setLayout(main_layout)
+        edit_widget.returnPressed.connect(self.add_task)
+
+    def add_task(self):
+        print("ca va")
+
+
+
 
 
 
